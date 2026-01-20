e913aa66b5faf3cb9c10ce52fe41a6621de5607a
Maximaam <maximaam@gmail.com>
2026-01-20 16:49:47 +0100

    update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Enum\Color;
+use App\Helper\ProductHelper;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/{_locale}/cart', name: 'app_cart_')]
 final class CartController extends AbstractController
@@ -16,8 +21,76 @@ final class CartController extends AbstractController
     #[Route('/', name: 'index', methods: [Request::METHOD_GET])]
     public function index(SessionInterface $session): Response
     {
+        $cart = $session->get('cart', []);
+
+        if ([] === $cart) {
+            return $this->redirectToRoute('app_index_index');
+        }
+
         return $this->render('cart/index.html.twig', [
-            'cart' => $session->get('cart', []),
+            'cart' => ProductHelper::computeCard($cart),
         ]);
+    }
+
+    #[Route('/add/{id}', name: 'add', methods: [Request::METHOD_POST])]
+    public function add(int $id, Request $request, SessionInterface $session, ProductRepository $productRepository, TranslatorInterface $translator): Response
+    {
+        $product = $productRepository->find($id)
+            ?? throw $this->createNotFoundException('Product not found');
+
+        $cart = $session->get('cart', []);
+        $quantity = $request->request->getInt('quantity');
+        $color = $request->request->get('color');
+        $size = $request->request->get('size');
+
+        if ($quantity <= 0 || !\in_array($color, Color::values(), true)) {
+            $this->addFlash(
+                'error',
+                $translator->trans('msg.error_cart_params')
+            );
+
+            return $this->redirectToRoute('app_index_catalogue', [
+                'catAlias' => $product->getCategory()->getParent(),
+                'subCatAlias' => $product->getCategory(),
+                'itemId' => $product->getId(),
+            ]);
+        }
+
+        $key = $id.'_'.$color.'_'.$size;
+        $itemUrl = $this->generateUrl(
+            'app_index_catalogue',
+            [
+                'catAlias' => $product->getCategory()->getParent()?->getAlias($request->getLocale()),
+                'subCatAlias' => $product->getCategory()->getAlias($request->getLocale()),
+                'itemId' => $product->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $cart[$key] = [
+            'id' => $id,
+            'title' => $product->getTitle($request->getLocale()),
+            'item_number' => $product->getItemNumber(),
+            'quantity' => $quantity,
+            'color' => $color,
+            'size' => $size,
+            'price' => $product->getPrice(),
+            'full_price' => $product->getPrice() * $quantity,
+            'item_url' => $itemUrl,
+            'image' => $product->getImages()[0]?->getImageName(),
+        ];
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart_index');
+    }
+
+    #[Route('/remove/{itemKey}', name: 'remove', methods: [Request::METHOD_GET])]
+    public function remove(string $itemKey, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart');
+        unset($cart[$itemKey]);
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart_index');
     }
 }
---
+++ src/Helper/ProductHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper;
+
+final readonly class ProductHelper
+{
+    public const int VAT = 19;
+
+    /**
+     * @param array<int, mixed> $cart
+     *
+     * @return array<int|string, mixed>
+     */
+    public static function computeCard(array $cart): array
+    {
+        $data = $result = [];
+        $total = 0;
+        foreach ($cart as $itemKey => $item) {
+            $total += (int) $item['full_price'];
+            $data[$itemKey] = $item;
+            $exclTax = round($total / ((self::VAT / 100) + 1), 2);
+            $result = [
+                'products' => $data,
+                'totals' => [
+                    'excl_tax' => $exclTax,
+                    'vat' => round($total - $exclTax, 2),
+                    'total' => $total,
+                ],
+            ];
+        }
+
+        return $result;
+    }
+}
