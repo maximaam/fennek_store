f94251038b1b00be8418e1176336e4122c916dba
Maximaam <maximaam@gmail.com>
2026-05-05 22:03:12 +0200

    fix tests

--- tests/Controller/Admin/DashboardControllerTest.php
+++ tests/Controller/Admin/DashboardControllerTest.php
@@ -4,15 +4,32 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller\Admin;
 
+use App\Tests\LoginUserTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class DashboardControllerTest extends WebTestCase
 {
+    use LoginUserTrait;
+
     public function testIndexIsRedirect(): void
     {
         $client = self::createClient();
         $client->request('GET', '/admin');
 
         self::assertResponseRedirects();
+    }
+
+    public function testIndexIsLogged(): void
+    {
+        $client = self::createClient();
+        $this->loginSuperAdmin($client);
+
+        $client->request('GET', '/admin');
+        self::assertResponseRedirects();
+
+        $client->followRedirect();
+        self::assertResponseIsSuccessful();
+        self::assertPageTitleContains('Produkte Liste');
+        self::assertSame('/admin/product', $client->getRequest()->getPathInfo());
     }
 }
