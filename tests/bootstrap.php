832d02cc5204bc3eb105275fddeb72dd58f00bb2
Maximaam <maximaam@gmail.com>
2026-05-12 16:17:07 +0200

    add tests fixtures

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(\dirname(__DIR__).'/.env');
+new Dotenv()->bootEnv(\dirname(__DIR__).'/.env');
 
 if ((bool) ($_SERVER['APP_DEBUG'] ?? false)) {
     umask(0o000);
